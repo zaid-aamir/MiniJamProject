4711794cceefc8bed0eba725b483be1a93250baf
izaan200913-beep <izaan200913@gmail.com>
2026-03-13 22:00:29 +0400

    Delete mini-jam directory

--- mini-jam/BatteryDrop.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var Parent: DropArea
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Interact") and Parent.IsPlayerThere:
-		Parent.GetObject().queue_free()
-	print(Parent.PlayerThatWillPickUp)
